ced2d2e5b5242a493a075514ec6dc9f577e5800f
413hy <hey.04138714@gmail.com>
2025-04-27 12:09:37 +0800

    img_001

--- server.js
+++ server.js
@@ -7,16 +7,25 @@ const PORT = process.env.PORT || 3000;
 // 存放图片的目录路径
 const imagesDir = path.join(__dirname, 'images');
 
-// 获取随机图片文件
-const getRandomImage = () => {
-  const files = fs.readdirSync(imagesDir);
+// 获取随机图片文件，根据设备类型选择不同的目录
+const getRandomImage = (userAgent) => {
+  let deviceType = 'computer'; // 默认设备类型为电脑
+  
+  // 检查 User-Agent 来识别设备类型
+  if (/mobile/i.test(userAgent)) {
+    deviceType = 'phone';  // 如果是手机
+  }
+
+  const deviceDir = path.join(imagesDir, deviceType);
+  const files = fs.readdirSync(deviceDir);
   const randomFile = files[Math.floor(Math.random() * files.length)];
-  return path.join(imagesDir, randomFile);
+  return path.join(deviceDir, randomFile);
 };
 
 // 路由：返回随机图片
 app.get('/', (req, res) => {
-  const imagePath = getRandomImage();
+  const userAgent = req.get('User-Agent');  // 获取请求头中的 User-Agent
+  const imagePath = getRandomImage(userAgent);
   res.sendFile(imagePath);
 });
 
